b62e1661bd2d67412b555a88401ea0e37eb5c81e
lgramu97 <gramuglialautaro@gmail.com>
2022-10-29 20:50:02 -0300

    Path Operation with path parameters

--- main.py
+++ main.py
@@ -6,10 +6,24 @@ app = FastAPI()
 # Path Operatorion Decorator.
 @app.get("/")
 # Path Operation Function
-def home(): 
+def home():
     """Main page.
 
     Returns:
         json: dictionary (json) hello world.
     """
-    return {"Hello" : "World"}
+    return {"Hello": "World"}
+
+
+# Path Operation with path Parameters.
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    """Example route with path parameter.
+
+    Args:
+        item_id (int): number of item.
+
+    Returns:
+        dictionary: json with the item id.
+    """
+    return {"item_id": item_id}
